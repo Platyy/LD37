b8c9c913acfcc9bcec03fe6889131273b4358024
Nick <nicholasdallafiore@hotmail.com>
2016-12-09 17:38:43 +1100

    + Added DOTween, added really simple example to NicksScene

---
+++ Assets/Scripts/MenuTween.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public class MenuTween : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler {
+   
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        transform.DOScale(new Vector3(2, 2, 2), .3f);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        transform.DOScale(new Vector3(1, 1, 1), .3f);
+    }
+
+}
