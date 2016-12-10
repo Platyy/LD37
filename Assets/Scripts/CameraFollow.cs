5354a52c0483312b6976d1e49692c175c9f052ab
Nick <nicholasdallafiore@hotmail.com>
2016-12-10 21:36:24 +1100

    + Added smooth camera follow

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+    public float m_SmoothTime;
+    private Vector3 m_RefVec = Vector3.zero;
+
+    public Transform m_Player;
+
+    void Update()
+    {
+        SetPosition();
+    }
+
+    void SetPosition()
+    {
+        Vector3 _target = m_Player.TransformPoint(new Vector3(0, 0, -20));
+        transform.position = Vector3.SmoothDamp(transform.position, _target, ref m_RefVec, m_SmoothTime);
+    }
+
+}
