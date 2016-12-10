767d810f71a48d91401fc3d57f0d35a83b780618
Nick <nicholasdallafiore@hotmail.com>
2016-12-10 15:40:14 +1100

    + Added walls that don't stick, added a dash, added push coral.

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,16 +1,95 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class PlayerController : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public float m_MoveSpeed = 1f;
+    public float m_JumpPower = 5f;
+    public float m_MaxVelocity = 2f;
+
+    public float m_DoubletapTime = 0.5f;
+    private float m_DTRemaining;
+    private int m_ButtonCount = 0;
+
+    private Rigidbody m_RB;
+    private Vector3 m_CurrentPosition;
+
+    
+    void Start()
+    {
+        m_RB = GetComponent<Rigidbody>();
+        m_DTRemaining = m_DoubletapTime;
+    }
+
+    void FixedUpdate()
+    {
+        Move();
+        Jump();
+        Dash();
+        m_RB.velocity = new Vector3(Mathf.Clamp(m_RB.velocity.x, -m_MaxVelocity, m_MaxVelocity), m_RB.velocity.y, m_RB.velocity.z);
+    }
+
+    void Move()
+    {
+        if (Input.GetKey(KeyCode.A)) // Left
+        {
+            m_RB.velocity = new Vector3(m_RB.velocity.x - m_MoveSpeed, m_RB.velocity.y, m_RB.velocity.z);
+        }
+        if(Input.GetKey(KeyCode.D)) // Right
+        {
+            m_RB.velocity = new Vector3(m_RB.velocity.x + m_MoveSpeed, m_RB.velocity.y, m_RB.velocity.z);
+        }
+    }
+
+    void Jump()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            m_RB.velocity = new Vector3(m_RB.velocity.x, m_RB.velocity.y + m_JumpPower, m_RB.velocity.z);
+        }
+    }
+
+    void Dash()
+    {
+        if(Input.GetKeyDown(KeyCode.A))
+        {
+            if(m_DTRemaining >= 0 && m_ButtonCount == 1)
+            {
+                Debug.Log("Dashed");
+                transform.DOMoveX(transform.position.x - 2, 0.2f);
+                m_DTRemaining = m_DoubletapTime;
+            }
+            else
+            {
+                m_DTRemaining = m_DoubletapTime;
+                m_ButtonCount += 1;
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            if (m_DTRemaining >= 0 && m_ButtonCount == 1)
+            {
+                Debug.Log("Dashed");
+                transform.DOMoveX(transform.position.x + 2, 0.2f);
+                m_DTRemaining = m_DoubletapTime;
+            }
+            else
+            {
+                m_DTRemaining = m_DoubletapTime;
+                m_ButtonCount += 1;
+            }
+        }
+
+        if (m_DTRemaining >= 0)
+        {
+            m_DTRemaining -= Time.deltaTime;
+        }
+        else
+        {
+            m_ButtonCount = 0;
+        }
+    }
+
 }
---
+++ Assets/Scripts/Traps/PushCoral.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PushCoral : MonoBehaviour {
+
+    public float m_ForceAmount = 50f;
+
+    public float m_BlowTime = 2f;
+    public float m_DownTime = 5f;
+    private float m_BlowTimeLeft;
+    private float m_DownTimeLeft;
+
+    private bool m_Blowing = false;
+
+
+    void Start()
+    {
+        m_BlowTimeLeft = m_BlowTime;
+        m_DownTimeLeft = m_DownTime;
+    }
+
+    void Update()
+    {
+        Blow();
+    }
+
+    void OnTriggerStay(Collider other)
+    {
+        if (other.tag == "Player" && m_Blowing)
+        {
+            other.GetComponentInParent<Rigidbody>().AddForce(transform.forward * m_ForceAmount, ForceMode.Acceleration);
+        }
+    }
+
+    void Blow()
+    {
+        if(!m_Blowing)
+        {
+            m_DownTimeLeft -= Time.deltaTime;
+            if(m_DownTimeLeft <= 0)
+            {
+                m_DownTimeLeft = m_DownTime;
+                m_Blowing = true;
+            }
+        }
+        else
+        {
+            m_BlowTimeLeft -= Time.deltaTime;
+            if (m_BlowTimeLeft <= 0)
+            {
+                m_BlowTimeLeft = m_BlowTime;
+                m_Blowing = false;
+            }
+        }
+    }
+
+}
